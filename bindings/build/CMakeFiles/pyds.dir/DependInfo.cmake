37c3f371823bd242be2bc6916eb54921c631546c
Ruijie <panruijie0820@163.com>
2025-11-04 09:56:53 +0000

    code : init

---
+++ bindings/build/CMakeFiles/pyds.dir/DependInfo.cmake
@@ -0,0 +1,52 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/opt/nvidia/deepstream/deepstream/sources/Deepstream_Python_Stack/bindings/src/bindanalyticsmeta.cpp" "/opt/nvidia/deepstream/deepstream/sources/Deepstream_Python_Stack/bindings/build/CMakeFiles/pyds.dir/src/bindanalyticsmeta.cpp.o"
+  "/opt/nvidia/deepstream/deepstream/sources/Deepstream_Python_Stack/bindings/src/bindfunctions.cpp" "/opt/nvidia/deepstream/deepstream/sources/Deepstream_Python_Stack/bindings/build/CMakeFiles/pyds.dir/src/bindfunctions.cpp.o"
+  "/opt/nvidia/deepstream/deepstream/sources/Deepstream_Python_Stack/bindings/src/bindgstnvdsmeta.cpp" "/opt/nvidia/deepstream/deepstream/sources/Deepstream_Python_Stack/bindings/build/CMakeFiles/pyds.dir/src/bindgstnvdsmeta.cpp.o"
+  "/opt/nvidia/deepstream/deepstream/sources/Deepstream_Python_Stack/bindings/src/bindmeta360.cpp" "/opt/nvidia/deepstream/deepstream/sources/Deepstream_Python_Stack/bindings/build/CMakeFiles/pyds.dir/src/bindmeta360.cpp.o"
+  "/opt/nvidia/deepstream/deepstream/sources/Deepstream_Python_Stack/bindings/src/bindnvbufsurface.cpp" "/opt/nvidia/deepstream/deepstream/sources/Deepstream_Python_Stack/bindings/build/CMakeFiles/pyds.dir/src/bindnvbufsurface.cpp.o"
+  "/opt/nvidia/deepstream/deepstream/sources/Deepstream_Python_Stack/bindings/src/bindnvdsinfer.cpp" "/opt/nvidia/deepstream/deepstream/sources/Deepstream_Python_Stack/bindings/build/CMakeFiles/pyds.dir/src/bindnvdsinfer.cpp.o"
+  "/opt/nvidia/deepstream/deepstream/sources/Deepstream_Python_Stack/bindings/src/bindnvdsmeta.cpp" "/opt/nvidia/deepstream/deepstream/sources/Deepstream_Python_Stack/bindings/build/CMakeFiles/pyds.dir/src/bindnvdsmeta.cpp.o"
+  "/opt/nvidia/deepstream/deepstream/sources/Deepstream_Python_Stack/bindings/src/bindnvosd.cpp" "/opt/nvidia/deepstream/deepstream/sources/Deepstream_Python_Stack/bindings/build/CMakeFiles/pyds.dir/src/bindnvosd.cpp.o"
+  "/opt/nvidia/deepstream/deepstream/sources/Deepstream_Python_Stack/bindings/src/bindopticalflow.cpp" "/opt/nvidia/deepstream/deepstream/sources/Deepstream_Python_Stack/bindings/build/CMakeFiles/pyds.dir/src/bindopticalflow.cpp.o"
+  "/opt/nvidia/deepstream/deepstream/sources/Deepstream_Python_Stack/bindings/src/bindpreprocessmeta.cpp" "/opt/nvidia/deepstream/deepstream/sources/Deepstream_Python_Stack/bindings/build/CMakeFiles/pyds.dir/src/bindpreprocessmeta.cpp.o"
+  "/opt/nvidia/deepstream/deepstream/sources/Deepstream_Python_Stack/bindings/src/bindroimeta.cpp" "/opt/nvidia/deepstream/deepstream/sources/Deepstream_Python_Stack/bindings/build/CMakeFiles/pyds.dir/src/bindroimeta.cpp.o"
+  "/opt/nvidia/deepstream/deepstream/sources/Deepstream_Python_Stack/bindings/src/bindschema.cpp" "/opt/nvidia/deepstream/deepstream/sources/Deepstream_Python_Stack/bindings/build/CMakeFiles/pyds.dir/src/bindschema.cpp.o"
+  "/opt/nvidia/deepstream/deepstream/sources/Deepstream_Python_Stack/bindings/src/bindtrackermeta.cpp" "/opt/nvidia/deepstream/deepstream/sources/Deepstream_Python_Stack/bindings/build/CMakeFiles/pyds.dir/src/bindtrackermeta.cpp.o"
+  "/opt/nvidia/deepstream/deepstream/sources/Deepstream_Python_Stack/bindings/src/custom_binding/bindcustom.cpp" "/opt/nvidia/deepstream/deepstream/sources/Deepstream_Python_Stack/bindings/build/CMakeFiles/pyds.dir/src/custom_binding/bindcustom.cpp.o"
+  "/opt/nvidia/deepstream/deepstream/sources/Deepstream_Python_Stack/bindings/src/pyds.cpp" "/opt/nvidia/deepstream/deepstream/sources/Deepstream_Python_Stack/bindings/build/CMakeFiles/pyds.dir/src/pyds.cpp.o"
+  "/opt/nvidia/deepstream/deepstream/sources/Deepstream_Python_Stack/bindings/src/utils.cpp" "/opt/nvidia/deepstream/deepstream/sources/Deepstream_Python_Stack/bindings/build/CMakeFiles/pyds.dir/src/utils.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "pyds_EXPORTS"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  "/opt/nvidia/deepstream/deepstream/sources/includes"
+  "/opt/nvidia/deepstream/deepstream/sources/gst-plugins/gst-nvdspreprocess/include"
+  "../include/bind"
+  "../include/nvds"
+  "../3rdparty/pybind11/include/pybind11"
+  "../3rdparty/pybind11/include"
+  "/usr/include/python3.12"
+  "/usr/include/gstreamer-1.0"
+  "/usr/include/glib-2.0"
+  "/usr/lib/aarch64-linux-gnu/glib-2.0/include"
+  "/usr/lib/x86_64-linux-gnu/glib-2.0/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ bindings/build/CMakeFiles/pyds.dir/cmake_clean.cmake
@@ -0,0 +1,25 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pyds.dir/src/bindanalyticsmeta.cpp.o"
+  "CMakeFiles/pyds.dir/src/bindfunctions.cpp.o"
+  "CMakeFiles/pyds.dir/src/bindgstnvdsmeta.cpp.o"
+  "CMakeFiles/pyds.dir/src/bindmeta360.cpp.o"
+  "CMakeFiles/pyds.dir/src/bindnvbufsurface.cpp.o"
+  "CMakeFiles/pyds.dir/src/bindnvdsinfer.cpp.o"
+  "CMakeFiles/pyds.dir/src/bindnvdsmeta.cpp.o"
+  "CMakeFiles/pyds.dir/src/bindnvosd.cpp.o"
+  "CMakeFiles/pyds.dir/src/bindopticalflow.cpp.o"
+  "CMakeFiles/pyds.dir/src/bindpreprocessmeta.cpp.o"
+  "CMakeFiles/pyds.dir/src/bindroimeta.cpp.o"
+  "CMakeFiles/pyds.dir/src/bindschema.cpp.o"
+  "CMakeFiles/pyds.dir/src/bindtrackermeta.cpp.o"
+  "CMakeFiles/pyds.dir/src/custom_binding/bindcustom.cpp.o"
+  "CMakeFiles/pyds.dir/src/pyds.cpp.o"
+  "CMakeFiles/pyds.dir/src/utils.cpp.o"
+  "pyds.pdb"
+  "pyds.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pyds.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
